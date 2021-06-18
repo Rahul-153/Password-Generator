f0bf8a244309bf3c8548da0c486aa097ff62238d
Rahul-153 <rathorerahul355@gmail.com>
2021-06-19 02:51:37 +0530

    changed the design of page and added few more features

--- password.js
+++ password.js
@@ -1,23 +1,24 @@
 generate = () => {
-    let lower = "abcdefghijklmnopqrstuvwxyz"
-    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let lower = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let num = "0123456789"
-    let special = "!@#$%^&*()~`+_-=|{}?><"
+    let special = "!@#$%^&*+_-=?"
     let size = parseInt(document.getElementById("length").value);
     let num_special = parseInt(document.getElementById("special").value);
+    let num_number = parseInt(document.getElementById("number").value);
     let password = ""
     let i;
     for (i = 0; i < num_special; i++) {
-        password += special.charAt(Math.random() * (size / 4));
+        password += special.charAt(Math.floor(Math.random() * special.length));
     }
-    for (i = 0; i < size - (num_special); i++) {
-        password += lower.charAt(Math.random() * (size / 4));
-        password += upper.charAt(Math.random() * (size / 4));
+    for (i = 0; i < num_number; i++) {
+        password += num.charAt(Math.floor(Math.random() * num.length));
+    }
+    for (i = 0; i < (size - (num_special + num_number)); i++) {
+        password += lower.charAt(Math.floor(Math.random() * lower.length));
     }
     password = password.split("").sort(function (a, b) { return 0.5 - Math.random() }).join("");
     document.getElementById("output").innerText = password;
 }
-show = () => {
-    let s = document.getElementById("length").value;
-    document.getElementsByClassName("len_value").innerText = "length is " + s
-}
+
+
+
